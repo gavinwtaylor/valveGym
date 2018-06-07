fe808db9dec6c56583c8ff21e9379d05a997b577
Gavin Taylor <taylor@usna.edu>
2018-06-07 14:15:32 +0200

    installing/importing correctly

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # valveGym
+
+For installation:
+
+1 - install tensorflow for python 3.5
+2 - in this dir, run `pip install .`
+3 - in your python code, import both gym, and valveGym
--- valveGym/envs/valve_env.py
+++ valveGym/envs/valve_env.py
@@ -1,14 +1,38 @@
 import gym
 from gym import error,spaces,utils
 from gym.utils import seeding
+import numpy as np
+from mpi4py import MPI
 
 class ValveEnv(gym.Env):
   metadata={'render.modes':['human']}
   def __init__(self):
-    pass
+    self.comm=MPI.COMM_WORLD
+    self.rank=self.comm.Get_rank()
+    self.status=MPI.Status()
+    self.action_space=spaces.Box(low=np.array([0,0]),high=np.array([2,20000]))
+    self.state_space=spaces.Box(low=np.array([0,310]),high=np.array([2,20000]))
   def step(self,action):
-    pass
+    assert self.action_space.contains(action)
+
+    self.comm.Send(action,partner,tag=1)
+
+    data=np.zeros((4,))
+    self.comm.Recv(data,status=status)
+    state=data[:2]
+    reward=data[2]
+    done=((data[3]-1.0)>-.001)
+    
+    return state,reward,done,None
+
+
   def reset(self):
-    pass
+    data=np.zeros((4,))
+    self.comm.Recv(data,status=status)
+    state=data[:2]
+    return state
+
   def render(self,mode='human',close=False):
+    pass
+  def seed(self):
     pass
