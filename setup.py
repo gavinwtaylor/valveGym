41d33cbc747185bfd01229f0445dbda79906a76b
Gavin Taylor <taylor@usna.edu>
2018-06-07 14:37:35 +0200

    just some bookkeeping

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='valveGym',
-      version='0.0.1',
-      install_requires=['gym','mpi4py'],  # And any other dependencies foo needs
-      packages=['valveGym','valveGym.envs'],
+NAME='valveGym'
+DESCRIPTION='for simulation of chemical plants'
+AUTHOR='Gavin Taylor and Tom Adams'
+
+setup(name=NAME,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    version='0.0.1',
+    install_requires=['gym','mpi4py'],  # And any other dependencies foo needs
+    packages=find_packages(),
 )  
